efd21f40684ed6586988ba9b8d68a09841dd66d2
Mark Kowalsky <markakowalsky@gmail.com>
2018-05-29 00:24:10 -0400

    Cleanup
    • seperated some big functions into other smaller functions
    • removed some unused variables
    • aesthetic modifications

--- sketch.js
+++ sketch.js
@@ -1,21 +1,17 @@
-var cnv;
-var colors = [];
-
-function centerCanvas() {
-    var x = (windowWidth - width) / 2;
-    var y = (windowHeight - height) / 2;
-    //cnv.position(x, y);
-}
+//ugly global vars
+var colors = [],
+    seededChance = new Chance(chance.hash({length: 4})),
+    hashes = [];
 
 function setup() {
-    
     console.log("onLoad");
     
+    //if there is no hash, make a new one!
     if (window.location.hash.substring(window.location.hash.lastIndexOf('/') + 1).length === 0) {
         generateHash();
-    } else {
+    } else { //or if a user specified one, load it instead
         console.log("Linked to pre-hashed page " + location.hash);
-        genArt(location.hash);
+        genData(location.hash);
     }
     
     //manage maximum canvas size
@@ -23,7 +19,8 @@ function setup() {
         cnv = createCanvas((window.innerWidth/1.45), (window.innerWidth/1.45));
     } else {
         cnv = createCanvas((window.innerHeight/1.45), (window.innerHeight/1.45));
-        }
+    }
+    
     cnv.parent('container');
     cnv.id('artboard');
     
@@ -31,30 +28,23 @@ function setup() {
     txt.style('color','colors[2]');
     txt.id('desc');
     txt.parent('container');
+    
+    tip = createDiv("press space to regenerate");
+    tip.style('color','#d9d9d9');
+    tip.id('tooltip');
+    tip.parent('container');
+    
     centerCanvas();
 }
 
-window.onresize = function() {
-    if (window.innerHeight > window.innerWidth){
-        var h = (window.innerWidth/1.2);
-    } else {
-        var h = (window.innerHeight/1.2);
-    }
-    
-    //h,h because it needs to remain square not rectangle. Whichever is smaller (w or h) .
-    resizeCanvas(h,h);
-    width = h;
-    height = h;
-}
-
 function draw() {
-    background(255, 255, 255);
+    background(230);
     noStroke();
     
     if (window.innerHeight > window.innerWidth){
-        var perc = map(window.innerWidth,400,displayWidth,.4,1);
+        perc = map(window.innerWidth, 400, displayWidth, .4, 1);
     } else {
-        var perc = map(window.innerHeight,200,displayHeight,.4,1);
+        perc = map(window.innerHeight, 200, displayHeight, .4, 1);
     }
     
     translate(width/2, width/2);
@@ -66,24 +56,53 @@ function draw() {
     select('#desc').html(see_v);
     select('#desc').style('color',colors[0]);
     
-    //DRAW PRACTICE SHAPES TO TEST COLORS
-    ellipse(255/-2,0,225,225);
-    fill(colors[1]);
-    ellipse(255/2,0,225,225);
-    fill(colors[2]);
-    ellipse(0,0,225,225);
+    rectMode(CENTER);
+    createShapes();
 
     //saveCanvas(cnv,toString(location.hash),'jpg')
 }
 
-//seeseededChance is only used to generate the url, then those values are used for repeatable URLs and Colors, sizes, etc.
-var seededChance = new Chance(chance.hash({length: 3})),
-    hashes = [],
-    hs,
-    latest,
-    s;
+//centering helper function
+function centerCanvas() {
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 2;
+}
 
-function genArt(g) {
+//window resize detection
+window.onresize = function() {
+    if (window.innerHeight > window.innerWidth) {
+        var h = (window.innerWidth/1.2);
+    } else {
+        var h = (window.innerHeight/1.2);
+    }
+    
+    //h,h because it needs to remain square not rectangle. Whichever is smaller (w or h) .
+    resizeCanvas(h,h);
+    width = h;
+    height = h;
+};
+
+//space bar detect / regenerate function
+window.onkeydown = function (e) {
+    switch (e.keyCode) {
+    case 32: //space
+        console.log("onkeyup");
+        colors = [];
+        generateHash();
+        tip.class("fade");
+        break;
+    }
+}
+
+//color helper function
+function colorHsluv(h, s, l) {
+    var rgb = hsluv.hsluvToRgb([h, s, l]);
+    return color(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
+}
+
+
+//main function that determines stats about art
+function genData(g) {
     var gen = new Chance(g);
     
     see_v = g,
@@ -92,7 +111,14 @@ function genArt(g) {
     gri_v = gen.bool(),
     pad_v = gen.bool(),
     siz_v = gen.bool(),
-    pos_v = gen.weighted(['Center', 'Top Left', 'Top Right', 'Bottom Right', 'Bottom Left'], [100, 10, 5, 5, 5]) ;
+    pos_v = gen.weighted(['Center', 'Top Left', 'Top Right', 'Bottom Right', 'Bottom Left'], [100, 10, 5, 5, 5]);
+    
+    //decide layout 1x2, 1x3, 1x4, 2x2, 2x3, 2x4, 3x3, 3x4
+    column_v = gen.integer({min: 1, max: 4});
+    row_v = gen.integer({min: 2, max: 5});
+    
+//    shape_gen = gen.weighted(['blank', 'rect', 'tri', 'circle'], [5, 2, 1, 2]);
+    shape_gen = gen.weighted(['rect', 'tri', 'circle'], [2, 1, 3]);
     
     //monochromatic stepper (saturation++)
     //triad (rand hue, 100 sat, 50 light)
@@ -122,47 +148,65 @@ function genArt(g) {
 
     //avoid super white
     if (colors[2] == "rgba(255,255,255,1)") {
-        colors[2] = "rgba(240,240,240,1)";
+        colors[2] = "rgba(238,238,238,1)";
     }
     
+    printStats();
+}
+
+//printer function
+function printStats() {
     print("seed: " + see_v);
     print("legs: " + leg_v);
     print("elements: " + ele_v);
     print("grid: " + gri_v);
     print("padding: " + pad_v);
-    print("oversized: " + siz_v);
+    print("oversized [off]: " + siz_v);
     print("color 1: " + colors[0]);
     print("color 2: " + colors[1]);
     print("color 3: " + colors[2]);
-    print("position: " + pos_v);
+    print("position [off]: " + pos_v);    
 }
 
+//fills each row/col with shapes
+function createShapes() {
+   for(var j = 0; j < row_v; j++) {
+        for(var k = 0; k < column_v; k++) {
+            fill(colors[0]);
+            switch(shape_gen) {
+                case "blank":
+                    //console.log(j + ", " + k + ": " + shape_gen);
+                    break;
+                case "circle":
+                    ellipse(j*30,k*30,30,30);
+                    //console.log(j + ", " + k + ": " + shape_gen);
+                    break;
+                case "rect":
+                    rect(j*30,k*30,31,31);
+                    //console.log(j + ", " + k + ": " + shape_gen);
+                    break;
+                case "tri":
+                    //x1,y1,x2,y2,x3,y3
+                    triangle((j*30)-15,((k)*30)-15,(j*30)-15,((k+1)*30)-15,((j+1)*30)-15,((k+1)*30)-15);
+                    //console.log(j + ", " + k + ": " + shape_gen);
+                    break;
+                default:
+                    break;
+            }
+        } 
+    }
+}
+
+//helper function for gen
 function generateHash() {
     location.hash = seededChance.hash({length: 5});
     console.log("new hash generated: " + location.hash);
-    hs = location.hash;
-    hashes.push(hs);
-    genArt(location.hash);
+    hashes.push(location.hash);
+    genData(location.hash);
 }
 
 //generates art based on the previous hash
 function sameHash() {
     var sHash = window.location.hash.substring(window.location.hash.lastIndexOf('#') + 1);
-    genArt(sHash);
-}
-
-window.onkeydown = function (e) {
-    switch (e.keyCode) {
-    case 32: //space
-        console.log("onkeyup");
-        colors = [];
-        generateHash();
-        //redraw();
-        break;
-    }
-};
-
-function colorHsluv(h, s, l) {
-    var rgb = hsluv.hsluvToRgb([h, s, l]);
-    return color(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
+    genData(sHash);
 }
