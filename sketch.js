62cfab979fcfe66193d84e2fcd66f5b5f4f5df9f
Mark Kowalsky <markakowalsky@gmail.com>
2018-06-24 04:36:08 -0400

    Relink description with hash/seed
    Modify hashing system

--- sketch.js
+++ sketch.js
@@ -1,17 +1,18 @@
-//ugly global vars
 var colors = [],
-    seededChance = new Chance(chance.hash({length: 4})),
-    hashes = [];
+    hashes = [],
+    artwork = [];
 
 function setup() {
     console.log("onLoad");
     
+    hashgenerator = new HashGenerator();
+    
     //if there is no hash, make a new one!
     if (window.location.hash.substring(window.location.hash.lastIndexOf('/') + 1).length === 0) {
-        generateHash();
+        hashgenerator.newHash();
     } else { //or if a user specified one, load it instead
         console.log("Linked to pre-hashed page " + location.hash);
-        genData(location.hash);
+        hashgenerator.recycleHash();
     }
     
     //manage maximum canvas size
@@ -24,7 +25,7 @@ function setup() {
     cnv.parent('container');
     cnv.id('artboard');
     
-    txt = createDiv(see_v);
+    txt = createDiv(hashgenerator.seed);
     txt.style('color','colors[2]');
     txt.id('desc');
     txt.parent('container');
@@ -33,6 +34,11 @@ function setup() {
     tip.style('color','#d9d9d9');
     tip.id('tooltip');
     tip.parent('container');
+    
+    //description
+    hashgenerator.seed = location.hash;
+    select('#desc').html(hashgenerator.seed);
+    select('#desc').style('color',colors[0]);
     
     centerCanvas();
 }
@@ -49,16 +55,13 @@ function draw() {
     
     translate(width/2, width/2);
     scale(perc);
-    
     fill(colors[0]);
-    
-    //description
-    select('#desc').html(see_v);
-    select('#desc').style('color',colors[0]);
-    
     rectMode(CENTER);
-    createShapes();
-
+    
+    for (cnt = 0; cnt < artwork.length; cnt++) {
+        artwork[cnt].display();
+    }
+    
     //saveCanvas(cnv,toString(location.hash),'jpg')
 }
 
@@ -83,65 +86,113 @@ window.onresize = function() {
 };
 
 //space bar detect / regenerate function
-window.onkeydown = function (e) {
+window.onkeydown = function(e) {
     switch (e.keyCode) {
     case 32: //space
         console.log("onkeyup");
         colors = [];
-        generateHash();
+        //artwork = [];
+        
+        hashgenerator.newHash();
+        if (artwork != []) {
+            artwork[0].display();
+        } else {
+            console.log("nothing to print!");
+        }
+            
+        //console.log()
         tip.class("fade");
         break;
     }
 }
 
-//color helper function
-function colorHsluv(h, s, l) {
-    var rgb = hsluv.hsluvToRgb([h, s, l]);
-    return color(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
+function addArtObject(a) {
+    artwork.push(new Artwork (chance.hash({length: 4})));
 }
 
+function HashGenerator() {
+    
+    this.managerSeed = new Chance(chance.hash({length: 4}));
+    
+    this.newHash = function() {
+        this.hashedName = this.managerSeed.hash({length: 5});
+        location.hash = this.hashedName;
+        this.seed = location.hash;
+        console.log("new hash generated: " + this.seed);
+        hashes.push(this.seed);
+        artwork = [];
+        addArtObject(this.seed);
+        console.log("added: " + this.seed);
+        
+        select('#desc').html(hashgenerator.seed);
+        select('#desc').style('color',colors[0]);
+    };
+    
+    this.recycleHash = function() {
+        this.seed = window.location.hash.substring(window.location.hash.lastIndexOf('#') + 1);
+        console.log("recycled seed: " + this.seed);
+        addArtObject(this.seed);
+    };
+    
+    return this.seed;
+}
+
+//function make2Darray(cols, rows) {
+//    var arr = new Array(cols);
+//    for (var i = 0; i < arr.length; i++) {
+//        arr[i] = new Array(rows);
+//    }
+//    return arr;
+//}
 
 //main function that determines stats about art
-function genData(g) {
-    var gen = new Chance(g);
+function Artwork(g) {
     
-    see_v = g,
-    leg_v = gen.integer({min: 2, max: 8}),
-    ele_v = gen.integer({min: 2, max: 10}),
-    gri_v = gen.bool(),
-    pad_v = gen.bool(),
-    siz_v = gen.bool(),
-    pos_v = gen.weighted(['Center', 'Top Left', 'Top Right', 'Bottom Right', 'Bottom Left'], [100, 10, 5, 5, 5]);
+    this.gen = new Chance(g);
+    
+    this.seed = g,
+    this.legs = this.gen.integer({min: 2, max: 8}),
+    this.elements = this.gen.integer({min: 2, max: 10}),
+    this.grid = this.gen.bool(),
+    this.padding = this.gen.bool(),
+    this.oversize = this.gen.bool(),
+    this.anchor = this.gen.weighted(['Center', 'Top Left', 'Top Right', 'Bottom Right', 'Bottom Left'], [100, 10, 5, 5, 5]);
     
     //decide layout 1x2, 1x3, 1x4, 2x2, 2x3, 2x4, 3x3, 3x4
-    column_v = gen.integer({min: 1, max: 4});
-    row_v = gen.integer({min: 2, max: 5});
+    this.columns = this.gen.integer({min: 2, max: 6});
+    this.rows = this.gen.integer({min: 2, max: 6});
     
-//    shape_gen = gen.weighted(['blank', 'rect', 'tri', 'circle'], [5, 2, 1, 2]);
-    shape_gen = gen.weighted(['rect', 'tri', 'circle'], [2, 1, 3]);
+    //this.shape = gen.weighted(['blank', 'rect', 'tri', 'circle'], [5, 2, 1, 2]);
+    this.shape = this.gen.n(this.gen.weighted, this.columns, ['blank', 'rect', 'tri', 'circle'], [3, 2, 1, 3]);
     
     //monochromatic stepper (saturation++)
     //triad (rand hue, 100 sat, 50 light)
     
-    var startHue = gen.integer({min: 0, max: 360});
-    var startSat = gen.integer({min: 50, max: 100});
-    var startLig = gen.integer({min: 30, max: 80});
-
-    var changeHue = gen.integer({min: 10, max: 100});
-    var changeSat = gen.integer({min: 15, max: 40});
-    var changeLig = gen.integer({min: 5, max: 20});
-
+    this.hue = this.gen.integer({min: 0, max: 360});
+    this.saturation = this.gen.integer({min: 50, max: 100});
+    this.lightness = this.gen.integer({min: 30, max: 80});
+    
+    //delta hue,sat,light
+    this.delta_hue = this.gen.integer({min: 10, max: 100});
+    this.delta_saturation = this.gen.integer({min: 15, max: 40});
+    this.delta_lightness = this.gen.integer({min: 5, max: 20});
+    
+    this.colorHsluv = function(h, s, l) {
+        var rgb = hsluv.hsluvToRgb([h, s, l]);
+        return color(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
+    };
+    
     for(var i = 0; i < 3; i++) {
-        if(startHue < 80) {
-            startSat = 100;
-            startLig = 50;
+        if(this.hue < 80) {
+            this.saturation = 100;
+            this.lightness = 50;
         }
         
       colors.push(
-        colorHsluv(
-          startHue + (i * changeHue),
-          startSat + (i * changeSat),
-          startLig + (i * changeLig)
+        this.colorHsluv(
+          this.hue + (i * this.delta_hue),
+          this.saturation + (i * this.delta_saturation),
+          this.lightness + (i * this.delta_lightness)
         )
       )
     }
@@ -151,62 +202,47 @@ function genData(g) {
         colors[2] = "rgba(238,238,238,1)";
     }
     
-    printStats();
-}
+    this.printStats = function(){
+        print("seed: " + this.seed);
+        print("legs: " + this.legs);
+        print("elements: " + this.elements);
+        print("grid: " + this.grid);
+        print("padding: " + this.padding);
+        print("oversized [off]: " + this.oversize);
+        print("color 1: " + colors[0]);
+        print("color 2: " + colors[1]);
+        print("color 3: " + colors[2]);
+        print("position [off]: " + this.anchor);
+    };
+    
+    this.display = function() {
 
-//printer function
-function printStats() {
-    print("seed: " + see_v);
-    print("legs: " + leg_v);
-    print("elements: " + ele_v);
-    print("grid: " + gri_v);
-    print("padding: " + pad_v);
-    print("oversized [off]: " + siz_v);
-    print("color 1: " + colors[0]);
-    print("color 2: " + colors[1]);
-    print("color 3: " + colors[2]);
-    print("position [off]: " + pos_v);    
-}
-
-//fills each row/col with shapes
-function createShapes() {
-   for(var j = 0; j < row_v; j++) {
-        for(var k = 0; k < column_v; k++) {
-            fill(colors[0]);
-            switch(shape_gen) {
-                case "blank":
-                    //console.log(j + ", " + k + ": " + shape_gen);
-                    break;
-                case "circle":
-                    ellipse(j*30,k*30,30,30);
-                    //console.log(j + ", " + k + ": " + shape_gen);
-                    break;
-                case "rect":
-                    rect(j*30,k*30,31,31);
-                    //console.log(j + ", " + k + ": " + shape_gen);
-                    break;
-                case "tri":
-                    //x1,y1,x2,y2,x3,y3
-                    triangle((j*30)-15,((k)*30)-15,(j*30)-15,((k+1)*30)-15,((j+1)*30)-15,((k+1)*30)-15);
-                    //console.log(j + ", " + k + ": " + shape_gen);
-                    break;
-                default:
-                    break;
-            }
-        } 
-    }
-}
-
-//helper function for gen
-function generateHash() {
-    location.hash = seededChance.hash({length: 5});
-    console.log("new hash generated: " + location.hash);
-    hashes.push(location.hash);
-    genData(location.hash);
-}
-
-//generates art based on the previous hash
-function sameHash() {
-    var sHash = window.location.hash.substring(window.location.hash.lastIndexOf('#') + 1);
-    genData(sHash);
+        for(var j = 0; j < this.rows; j++) {
+            for(var k = 0; k < this.columns; k++) {
+                switch(this.shape[j]) {
+                    case "blank":
+                        //console.log(j + ", " + k + ": " + shape_gen);
+                        break;
+                    case "circle":
+                        fill(this.gen.pickone(colors));
+                        ellipse(j*30,k*30,30,30);
+                        //console.log(j + ", " + k + ": " + shape_gen);
+                        break;
+                    case "rect":
+                        fill(this.gen.pickone(colors));
+                        rect(j*30,k*30,31,31);
+                        //console.log(j + ", " + k + ": " + shape_gen);
+                        break;
+                    case "tri":
+                        fill(this.gen.pickone(colors));
+                        //x1,y1,x2,y2,x3,y3
+                        triangle((j*30)-15,((k)*30)-15,(j*30)-15,((k+1)*30)-15,((j+1)*30)-15,((k+1)*30)-15);
+                        //console.log(j + ", " + k + ": " + shape_gen);
+                        break;
+                    default:
+                        break;
+                }
+            } 
+        }
+    };
 }
